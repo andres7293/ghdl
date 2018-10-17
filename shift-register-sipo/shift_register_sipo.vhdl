0a494049d1b17e886a7a3932a5b5ec091cf06016
andres <andres7293@gmail.com>
2018-10-17 17:58:26 +0100

    Reimplementation shift register with * Low active reset, rising edge clock. * Low active enable, rising edge clock. * LSB First

--- shift-register-sipo/shift_register_sipo.vhdl
+++ shift-register-sipo/shift_register_sipo.vhdl
@@ -1,29 +1,41 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--Clocked Shift register LSB first with:
+-- * low active reset, rising edge
+-- * low active enable, rising edge
 
 entity shift_register_sipo is
+        generic (n : integer := 8);
         port (
+                    reset        : in  std_logic;
+                    enable       : in  std_logic;
                     clock        : in  std_logic;
                     serial_in    : in  std_logic;
-                    parallel_out : out std_logic_vector(7 downto 0)
+                    parallel_out : out std_logic_vector(n-1 downto 0)
              );
-end shift_register_sipo;
+end entity;
 
 
 architecture Behavioural of shift_register_sipo is
 
-    signal Q : std_logic_vector(7 downto 0) := "00000000";
+    signal D : std_logic_vector(n-1 downto 0);
+    signal Q : std_logic_vector(n-1 downto 0);
 
 begin
 
     process (clock)
     begin
-        if rising_edge(clock) then
-            Q(7) <= serial_in;
-            Q(6 downto 0) <= Q(7 downto 1);
+        if (clock'event and (clock = '1')) then
+            Q <= D;
         end if;
     end process;
 
-    parallel_out(7 downto 0) <= Q(7 downto 0);
+    D <= Q(n-2 downto 0) & serial_in when (enable = '0') else
+         std_logic_vector(to_signed(0, n)) when (reset = '0') else
+         Q;
 
-end Behavioural;
+    parallel_out <= Q;
+
+end architecture;
--- shift-register-sipo/shift_register_sipo_tb.vhdl
+++ shift-register-sipo/shift_register_sipo_tb.vhdl
@@ -8,20 +8,27 @@ end shift_register_sipo_tb;
 architecture tb of shift_register_sipo_tb is
 
     component shift_register_sipo
+        generic (n : integer := 8);
         port (
+                    reset        : in  std_logic;
+                    enable       : in  std_logic;
                     clock        : in  std_logic;
                     serial_in    : in  std_logic;
-                    parallel_out : out std_logic_vector(7 downto 0)
+                    parallel_out : out std_logic_vector(8-1 downto 0)
              );
     end component;
 
+    signal reset        : std_logic;
+    signal enable       : std_logic;
     signal clock        : std_logic;
     signal serial_in    : std_logic;
-    signal parallel_out : std_logic_vector(7 downto 0);
+    signal parallel_out : std_logic_vector(8-1 downto 0);
 
     begin
         --Instantiate the unit under test
         uut : shift_register_sipo port map(
+                        reset        => reset,
+                        enable       => enable,
                         clock        => clock,
                         serial_in    => serial_in,
                         parallel_out => parallel_out
@@ -29,23 +36,45 @@ architecture tb of shift_register_sipo_tb is
 
       process
       begin
-          --falling edge
-          clock <= '0';
-          serial_in <= '0';
+
+          --Reset biestable
+          clock  <= '0';
+          reset  <= '0';
+          enable <= '1';
           wait for 1 ns;
-          --rising edge
           clock <= '1';
-          serial_in <= '1';
+          reset  <= '1';
           wait for 1 ns;
 
-          --falling edge
-          clock <= '0';
-          serial_in <= '0';
-          wait for 1 ns;
-          --rising edge
-          clock <= '1';
-          serial_in <= '1';
-          wait for 1 ns;
+          --shift bits to logic 1
+          for i in 0 to 8-1 loop
+              clock <= '0';
+              enable <= '0';
+              serial_in <= '1';
+              wait for 1 ns;
+              clock <= '1';
+              wait for 1 ns;
+          end loop;
+
+          --shift bits to logic 0
+          for i in 0 to 8-1 loop
+              clock <= '0';
+              enable <= '0';
+              serial_in <= '0';
+              wait for 1 ns;
+              clock <= '1';
+              wait for 1 ns;
+          end loop;
+
+          --shift bits to logic 1 with enable = 1
+          for i in 0 to 8-1 loop
+              clock <= '0';
+              enable <= '1';
+              serial_in <= '1';
+              wait for 1 ns;
+              clock <= '1';
+              wait for 1 ns;
+          end loop;
 
           wait;
       end process;
