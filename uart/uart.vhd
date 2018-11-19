083a48b98e7b09996b731a82d9fe985fd2b0d112
andres <andres7293@gmail.com>
2018-11-19 22:15:04 +0000

    Mux signals

--- uart/uart.vhd
+++ uart/uart.vhd
@@ -19,23 +19,43 @@ architecture rtl of uart is
     type TxLogicStates is (idle, load_registers, send_bit, reload_baudrate);
     signal D_TxState, Q_TxState : TxLogicStates;
 
-    signal D_cnt      : std_logic_vector(32-1 downto 0);
-    signal Q_cnt      : std_logic_vector(32-1 downto 0);
-    signal D_Tx       : std_logic;
-    signal Q_Tx       : std_logic;
-    signal D_shiftReg : std_logic_vector(8-1 downto 0);
-    signal Q_shiftReg : std_logic_vector(8-1 downto 0);
-    signal bit_time   : std_logic;
+    signal D_counterBus : std_logic_vector(32-1 downto 0);
+    signal Q_counterBus : std_logic_vector(32-1 downto 0);
+    signal D_Tx         : std_logic;
+    signal Q_Tx         : std_logic;
+    signal D_shiftReg   : std_logic_vector(8-1 downto 0);
+    signal Q_shiftReg   : std_logic_vector(8-1 downto 0);
+    signal bit_time     : std_logic;
+
+    --mux signals
+    signal baud_gen_mux     : std_logic;
+    signal uart_data_mux    : std_logic_vector(2 downto 0);
+    signal uart_output_mux  : std_logic_vector(2 downto 0);
 begin
 
     process (clock)
     begin
         if (clock'event and (clock = '1')) then
-            Q_cnt      <= D_cnt;
-            Q_shiftReg <= D_shiftReg;
-            Q_Tx       <= D_Tx;
-            Q_TxState  <= D_TxState;
+            Q_counterBus <= D_counterBus;
+            Q_shiftReg   <= D_shiftReg;
+            Q_Tx         <= D_Tx;
+            Q_TxState    <= D_TxState;
         end if;
     end process;
+
+    --control baud rate generator
+    D_counterBus <= baud when (baud_gen_mux = '1') else
+             std_logic_vector(unsigned(Q_counterBus) - 1);
+
+    --control load data into shiftRegister
+    D_shiftReg <= data when (uart_data_mux = "10") else
+                  Q_shiftReg when (uart_data_mux = "00");
+
+    --control output biestable bit
+    D_Tx <= '1' when (uart_data_mux = "11") else
+            Q_Tx when (uart_data_mux = "10");
+
+    --output bit
+    Tx <= Q_Tx;
 
 end architecture;
